ca8dfc0a6f13e661472b8f3acff484ea2af196ee
さとしん <131766015+satoshin-des@users.noreply.github.com>
2024-03-27 15:45:07 +0900

    Add files via upload

--- NewGame/Program.cs
+++ NewGame/Program.cs
@@ -11,12 +11,29 @@ namespace NewGame
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
+        public static ApplicationContext main_form;
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            // Application.Run(new PlayScreen());
+            main_form = new ApplicationContext();
+            main_form.MainForm = new PlayScreen();
+            Application.Run(main_form);
+        }
+
+        public static void DisplayPlayScreen()
+        {
+            main_form.MainForm = new PlayScreen();
+            main_form.MainForm.Show();
+        }
+        //5,Form1に切り替える処理
+        public static void DisplayResultScreen()
+        {
+            main_form.MainForm = new ResultScreen();
+            main_form.MainForm.Show();
         }
     }
 }
